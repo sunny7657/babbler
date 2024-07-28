46a25cbaa26e6923c045126503644aa475d4e11c
sunny7657 <ksvetlana466@gmail.com>
2024-07-28 22:41:16 +0200

    add input and tabs components

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,11 @@
+const AuthPage = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center text-opacity-90">
+      <div className="w-[80vw] h-[60vw] bg-blue-100 shadow-2xl rounded-3xl flex justify-center items-center">
+        <h1 className="text-5xl font-bold">Welcome</h1>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
