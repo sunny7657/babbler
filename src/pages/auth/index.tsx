0f91046f991e31894bc98eacc1bffc2941648266
sunny7657 <ksvetlana466@gmail.com>
2024-07-29 21:41:13 +0200

    add favicon and background images

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,19 @@
 const AuthPage = () => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center text-opacity-90">
-      <div className="w-[80vw] h-[60vw] bg-blue-100 shadow-2xl rounded-3xl flex justify-center items-center">
-        <h1 className="text-5xl font-bold">Welcome</h1>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center text-opacity-90 ">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <img
+          src="/src/assets/bg-image-1.png"
+          alt="background image talking girl"
+        />
+        <img
+          src="/src/assets/bg-image-4.png"
+          alt="background image talking girl"
+        />
+        <img
+          src="/src/assets/bg-image-5.png"
+          alt="background image talking girl"
+        />
       </div>
     </div>
   );
